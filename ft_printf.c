8ad85ea6f28364688869dea6e0351e8744bdb7b0
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-06-05 18:12:09 -0300

    nothing - just backup

--- ft_printf.c
+++ ft_printf.c
@@ -7,20 +7,23 @@ int	count_params(const char *template)
 	c = 0;
 	while(*template)
 	{
-		if(*template == "%")
+		if(*template == '%')
 			c ++;
 		template++;
 	}
-
+	return (c);
 }
 int	ft_printf(const char *template, ...)
 {
-	if (template)
+	// if (template)
 		return (count_params(template));
 }
 
 int main(void)
 {
-	int i = ft_printf("a%db%c");
-	
+	int i = ft_printf("a%db%cc%s");
+	char	*I = ft_itoa(i);
+	ft_putendl_fd(I, 1);
+	free(I);
+	return (0);
 }
