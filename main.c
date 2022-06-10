1f51d47ccfa4af6b4590a1d79523209c25e12dfd
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-06-10 05:07:04 +0200

    update main test

--- main.c
+++ main.c
@@ -1,109 +1,25 @@
 #include <stdio.h>
 #include "ft_printf.h"
 
-void	printgeral()
+// # define P ("%s, %s %d%%%c\n%i %x %X\n", "ola", "mundo", 42, '!', 24, 427, 347);
+# define P ("%uabc\n", -15);
+
+void	mymain()
 {
 	int i;
-	char	*I;
-
-	i = ft_printf("print int(D): {%d}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print int(I): {%i}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print char: {%c}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print string: {%s}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print pointer: {%p}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print miniHex: {%x}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print BigHex: {%X}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-
-	i = ft_printf("print porcent: {%%}\n");
-	I = ft_itoa(i);
-	ft_putendl_fd(I, 1);
-	free(I);
-}
-
-void	soma(int n, ...)
-{
-	va_list	args;
-	int		s;
-	va_start(args, n);
-
-	s = 0;
-	for (int i = 0; i < n; i++)
-		s += va_arg(args, int);
-
-	va_end(args);
-	printf("%d\n", s);
-}
-
-void	testwhitslpit()
-{
-	char	*temp;
-	char	*str;
-	int		i;
-	int		s;
-
-	i = 0;
-	str = "abc_{%d}_def_{%c}_ghi";
-	while (str[i] && str[i] != '%')
-		i++;
-	temp = ft_substr(str, 0, i - 0);
-	ft_putstr_fd("\n", 1);
-	ft_putstr_fd(temp, 1);
-	ft_putstr_fd("\n", 1);
-	free(temp);
-	printf("%d\n", i);
-	i++;
-	s = i;
-	while (str[i] && str[i] != '%')
-		i++;
-	temp = ft_substr(str, s, i - s);
-	ft_putstr_fd(temp, 1);
-	ft_putstr_fd("\n", 1);
-	free(temp);
-	printf("%d\n", i);
-}
-int	main(void)
-{
-	// int i;
 	// char	*I;
+	char	*str;
 
 	// i = ft_printf("%% %x %X %c %s %p %d %i %u %D", 'a', "bc", "bc", 4, 4, 4);
-	// d c i % -> u -> x, X -> s -> p
-	ft_printf("abc %% %i def %c ghi\n", 6, 't' + 256);
-	printf("abc %% %i def %c ghi\n", 6, 't' + 256);
-	// I = ft_itoa(i);
-	// ft_putendl_fd(I, 1);
-	// free(I);
-	//printgeral();
-	//printf("{%%} - {%d} - {% d} - {%-d} - {%-10d} - {%10d}", 3, 3, 3, 3, 3);
-	// printf("\n");
-	// soma(2, 2, 3);
-	// testwhitslpit();
+	// d c i % s x X -> u -> p
+	i = ft_printf P
+	printf("----\ntamanho que retorna: %d\n----\n", i);
+	i = printf P
+	printf("tamanho correto: %d\n", i);
+}
+
+int	main(void)
+{
+	mymain();
 	return (0);
 }
