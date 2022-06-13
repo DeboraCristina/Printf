8471e30875c06a015ad88847406d127f35006344
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-06-13 03:24:24 +0200

    test

---
+++ rain.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "ft_printf.h"
+
+// # define P ("%s, %s %d%%%c\n%i %x %X\n", "ola", "mundo", 42, '!', 24, 427, 347);
+# define P ("%p\n", p);
+
+void	mymain()
+{
+	int	i;
+	int	a;
+	int	*p;
+
+	a = 5;
+	p = &a;
+
+	i = ft_printf P
+	printf("----\ntamanho que retorna: %d\n----\n", i);
+	i = printf P
+	printf("tamanho correto: %d\n", i);
+}
+
+int	main(void)
+{
+	mymain();
+	return (0);
+}
