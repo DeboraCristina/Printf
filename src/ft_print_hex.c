ec1a7155d9a06efefdbe10ec96aadf959711a01b
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-06-15 16:25:54 -0300

    hexa and unsigned int protected

--- src/ft_print_hex.c
+++ src/ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: desilva <dede-2231@hotmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:30:42 by desilva           #+#    #+#             */
-/*   Updated: 2022/06/15 18:28:07 by desilva          ###   ########.fr       */
+/*   Updated: 2022/06/15 16:02:48 by desilva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	ft_print_hex(int placeholder, int i)
 	int		len;
 	char	*s;
 
+	if (i == 0)
+	{
+		ft_putchar_fd('0', 1);
+		return (1);
+	}
 	s = ft_u2hex(i, placeholder);
 	ft_putstr_fd(s, 1);
 	len = ft_strlen(s);
