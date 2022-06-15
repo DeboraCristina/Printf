e345ec5251225db5fe7510d8d4a77fc072690348
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-06-15 03:17:43 +0200

    fix

--- src/ft_print_ptr.c
+++ src/ft_print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: desilva <dede-2231@hotmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:30:55 by desilva           #+#    #+#             */
-/*   Updated: 2022/06/13 18:31:01 by desilva          ###   ########.fr       */
+/*   Updated: 2022/06/15 02:34:04 by desilva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,19 @@ int	ft_print_ptr(unsigned long int i)
 	int		len;
 	char	*s;
 
-	s = ft_tohex(i, 'x');
-	ft_putstr_fd("0x", 1);
-	ft_putstr_fd(s, 1);
-	len = ft_strlen(s) + 2;
-	if (s)
-		free(s);
+	if (i == 0)
+	{
+		ft_putstr_fd("(nil)", 1);
+		len = 5;
+	}
+	else
+	{
+		s = ft_tohex(i, 'x');
+		ft_putstr_fd("0x", 1);
+		ft_putstr_fd(s, 1);
+		len = ft_strlen(s) + 2;
+		if (s)
+			free(s);
+	}
 	return (len);
 }
--- src/ft_print_str.c
+++ src/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: desilva <dede-2231@hotmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:31:03 by desilva           #+#    #+#             */
-/*   Updated: 2022/06/13 18:31:10 by desilva          ###   ########.fr       */
+/*   Updated: 2022/06/15 02:34:06 by desilva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,15 @@ int	ft_print_str(char *s)
 {
 	int		len;
 
-	ft_putstr_fd(s, 1);
-	len = ft_strlen(s);
+	if (!s)
+	{
+		ft_putstr_fd("(null)", 1);
+		len = 6;
+	}
+	else
+	{
+		ft_putstr_fd(s, 1);
+		len = ft_strlen(s);
+	}
 	return (len);
 }
